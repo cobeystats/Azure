68dd4650106b78affae068464d54b3d9e2fa188c
Cobey Errett <errecob@statscan.com>
2017-10-03 12:05:10 +0000

    Initial Commit

---
+++ templatedeploy.ps1
@@ -0,0 +1,3 @@
+Login-AzureRmAccount
+
+New-AzureRmResourceGroupDeployment -TemplateFile C:\Users\errecob\Documents\Azure\arm-templates\101-vm-simple-windows\azuredeploy.json -TemplateParameterFile C:\Users\errecob\Documents\Azure\arm-templates\101-vm-simple-windows\azuredeploy.parameters.json
